34be414f2abbbe6a95dd2b4515bc99f094c6ad31
Afrin Siddiqui <56950345+afrin7@users.noreply.github.com>
2021-02-28 14:02:05 +0530

    Add files via upload

---
+++ ineuron_assignment2.py
@@ -0,0 +1,30 @@
+#Question 1
+'''str=""
+i=0
+print(str+"*")
+str=""
+j=0
+for i in range(3):
+     str=str+"*"
+     for k in range(i):
+          str=str+"*"
+     str=str+"*"
+     print(str)
+     j=0
+     k=0
+     str=""
+for i in range(4):
+     str=str+"*"
+     for k in range(4-i):
+          str=str+"*"
+     print(str)
+     j=0
+     k=0
+     str=""
+print(str+"*")'''
+
+#Question 2
+'''
+str=[]
+str=input("Enter a string: ")
+print(str[::-1])'''
